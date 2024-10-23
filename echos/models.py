075f98dab17ac0e77f038ee24db9ef1d9148d307
alu <mdiogcasko@gmail.com>
2024-10-23 20:25:38 +0100

    se añaden las FK

--- echos/models.py
+++ echos/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.db import models
 
 
@@ -6,3 +7,4 @@ class Echos(models.Model):
     content = models.TextField(default='', blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True)
